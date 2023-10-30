768a997d54c4710b14bb9e6d46bf47cfeb5a83c5
enginkavas <148543992+enginkavas@users.noreply.github.com>
2023-10-30 22:17:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,37 @@
 # app.py
-from flask import Flask, request, jsonify
-from sklearn.ensemble import RandomForestClassifier
+from flask import Flask, render_template, request
+import numpy as np
+from sklearn.linear_model import LinearRegression
 
 app = Flask(__name)
 
-# Create and train a simple ML model
-model = RandomForestClassifier()
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-# Define a simple endpoint for predictions
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.json
-    prediction = model.predict([data['features']])
-    return jsonify({'prediction': prediction.tolist()})
+    if request.method == 'POST':
+        try:
+            # Get the input value from the form
+            input_value = float(request.form['input_value'])
+
+            # Sample data
+            X = np.array([1, 2, 3, 4, 5]).reshape(-1, 1)
+            y = np.array([2, 4, 5, 4, 5])
+
+            # Create a linear regression model
+            model = LinearRegression()
+            model.fit(X, y)
+
+            # Predict using the input value
+            prediction = model.predict(np.array([[input_value]]))
+
+            return render_template('result.html', input_value=input_value, prediction=prediction[0])
+
+        except Exception as e:
+            return render_template('error.html', error_message=str(e))
 
 if __name__ == '__main__':
     app.run(debug=True)
+
