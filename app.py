3656a91932288c111d6b5c9abc83b923742fb22a
enginkavas <148543992+enginkavas@users.noreply.github.com>
2023-10-31 17:36:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,34 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        try:
+            # Get the input value from the form
+            input_value = float(request.form['input_value'])
+
+            # Sample data
+            X = np.array([1, 2, 3, 4, 5]).reshape(-1, 1)
+            y = np.array([2, 4, 5, 4, 5])
+
+            # Create a linear regression model
+            model = LinearRegression()
+            model.fit(X, y)
+
+            # Predict using the input value
+            prediction = model.predict(np.array([[input_value]]))
+
+            return render_template('result.html', input_value=input_value, prediction=prediction[0])
+
+        except Exception as e:
+            return render_template('error.html', error_message=str(e))
+
+
+
+
+
+
 # @app.route("/")
 # def hello():
 #    return "<h1>Hello MIS604 Class !</h1>"
