150a1563c9d18ad1fe323c67c413119e6cf02874
enginkavas <148543992+enginkavas@users.noreply.github.com>
2023-10-31 11:51:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,8 @@
 # app.py
-from flask import Flask, render_template, request
-# import numpy as np
-# from sklearn.linear_model import LinearRegression
-
-app = Flask(__name)
-
+from flask import Flask
+app = Flask(__name__)
 
 @app.route("/")
-    
-
-# def index():
-#    return render_template('index.html')
-
 def hello():
     return "<h1>Hello MIS604 Class !</h1>"
-
 
